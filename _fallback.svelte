6b53158da8093d7a2ed14573ace674894c479d05
sandy saputra <saputras038@gmail.com>
2020-10-14 21:15:41 +0700

    Create _fallback.svelte

---
+++ _fallback.svelte
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>pateron.id</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+  </head>
+  <style>
+  html, body {
+    background: #EFF3F4;
+    font-family: 'Open Sans', serif;
+    height: 100%;
+}
+
+.nav-bar-burger-color {
+    color: #ffffff;
+}
+
+.nav-bar-menu-bg-color {
+    background-color: transparent !important;
+}
+
+.hero-body .container {
+    max-width: 1000px;
+}
+
+.features {
+    margin-top: 0px;
+    padding: 2rem 0;
+}
+
+.card-content .content {
+    font-size: 15px;
+    margin: 1rem 1rem;
+}
+
+.card {
+    box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.18);
+}
+
+.intro {
+    padding: 5rem 0;
+    text-align: center;
+}
+
+.is-shady {
+    animation: flyintocenter .4s backwards;
+    background: #fff;
+    box-shadow: rgba(0, 0, 0, .1) 0 1px 0;
+    border-radius: 4px;
+    display: inline-block;
+    margin: 10px;
+    position: center;
+    transition: all .2s ease-in-out;
+}
+
+    .is-shady:hover {
+        box-shadow: 0 10px 16px rgba(0, 0, 0, .13), 0 6px 6px rgba(0, 0, 0, .19);
+    }
+
+.author-image {
+    position: absolute;
+    top: -105px;
+    left: 50%;
+    width: 210px;
+    height: 210px;
+    margin-left: -105px;
+    border: 3px solid #fafafa;
+    border-radius: 50%;
+}
+
+.picture-border-light {
+    border: 10px solid #ffffff;
+}
+
+.picture-border-dark {
+    border: 10px solid #fafafa;
+}
+
+.title-dark {
+    color: #363636 !important;
+}
+
+.margin-top-one-hundred {
+    margin-top: 100px !important;
+}
+
+.margin-top-neg-sixty {
+    margin-top: -60px;
+}
+
+.text-area-min-height {
+    min-height: 150px;
+}
+
+.heart-black {
+    color: deeppink;
+}
+
+.has-bg-img {
+    background: url('page1.jpg')center center !important;
+    background-size: cover !important;
+}
+
+.map-marker-red {
+    color: #ea4335;
+}
+
+.map-size {
+    height: 70%;
+}
+  </style>
+  <body>   
+<section class="hero">
+    <div class="hero-head">
+        <nav class="navbar">
+            <div class="container">
+                <div class="navbar-brand">
+                    <a class="navbar-item has-text-weight-semibold nav-bar-menu-bg-color" href="#">
+                    </a>
+                    <span class="navbar-burger burger nav-bar-burger-color" data-target="navbarMenu">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </span>
+                </div>
+                <div id="navbarMenu" class="navbar-menu nav-bar-menu-bg-color">
+                    <div class="navbar-end">
+                        <div class="tabs is-right">
+                            <ul>
+                                <li><a href="#pateron school">Pateron School</a></li>
+                               <li><a href="#Pateron Olym">Pateron Olym</a></li>
+                                <li><a href="#pateron.id">Pateron TPB</a></li>
+                                <li><a href="#pateron Blog">Pateron Blog</a></li>
+                                <li><a href="#pateron store">Pateron Store</a></li>
+                             
+                            </ul>
+                          
+                         
+                         <ul>
+                        <a class="button">Masuk</a>
+                        <a class="button">Gabung Gratis</a>
+                         </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</section>
+
+  <section class="section is-medium">
+    <div class="container">
+      <div class="columns is-vcentered">
+        <div class="column has-text-centered">
+          <h1class="title">404 Page Not Found</h1>
+          <p class="subtitle">Opps, Halaman tidak di temukan.</p>
+     
+          <a class="button">back to home</a>
+        </div>
+        <div class="column has-text-centered">
+          <img src="page1.jpg" />
+        </div>
+      </div>
+    </div>
+  </section>
+  </body>
+</html>
